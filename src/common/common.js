f59eece34522b6c2870fb8afac3aa65e313c93a0
LowLoop <huangjiefor@sina.com>
2017-08-21 14:09:56 +0800

    test

--- src/common/common.js
+++ src/common/common.js
@@ -1,10 +1,10 @@
 /**
  * Created by j.frank on 2017/7/17 0017.
  */
-/*var reqHost = "http://39.108.104.153:8888";
-var selfHost = "http://39.108.104.153:3000";*/
-var reqHost = "http://localhost:8888";
-var selfHost = "http://localhost:3000";
+var reqHost = "http://39.108.104.153:8888";
+var selfHost = "http://39.108.104.153:3000";
+/*var reqHost = "http://localhost:8888";
+var selfHost = "http://localhost:3000";*/
 
 module.exports = {
   getHost:function(hostName){
